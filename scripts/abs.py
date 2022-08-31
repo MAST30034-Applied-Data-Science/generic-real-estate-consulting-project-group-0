5dd740d4fc03ea79d51b8cefa33af93e90374136
Anthony He <deviltonyho@gmail.com>
2022-08-31 12:56:57 +1000

    update abs download file
    scrape.py is also updated according to discord by lucas

--- scripts/abs.py
+++ scripts/abs.py
@@ -3,12 +3,29 @@ import requests
 import zipfile
 import io
 import os
+from urllib.request import urlretrieve
 
-if not os.path.exists('../data/raw/abs_SA2_District_Boundaries'):
-    os.makedirs('../data/raw/abs_SA2_District_Boundaries')
-    
+# files (raw data) stored in ../data/raw
+output_relative_dir = '../data/raw/'
+# in this particular script, all files are from ABS 
+# hence they are stored in the following directory
+if not os.path.exists(output_relative_dir + 'abs'):
+    os.makedirs(output_relative_dir + 'abs')
+output_relative_dir = output_relative_dir + 'abs/'
+
+# download SA2 district boundaries information
+if not os.path.exists(output_relative_dir + 'abs_SA2_District_Boundaries'):
+    os.makedirs(output_relative_dir + 'abs_SA2_District_Boundaries')
 url = 'https://www.abs.gov.au/statistics/standards/australian-statistical-geography-standard-asgs-edition-3/jul2021-jun2026/access-and-downloads/digital-boundary-files/SA2_2021_AUST_SHP_GDA2020.zip'
 r = requests.get(url)
 z = zipfile.ZipFile(io.BytesIO(r.content))
-z.extractall('../data/raw/abs_SA2_District_Boundaries')
+z.extractall(output_relative_dir + 'abs_SA2_District_Boundaries')
 
+# download income statistics from ABS
+urlretrieve('https://www.abs.gov.au/statistics/labour/earnings-and-working-conditions/personal-income-australia/2014-15-2018-19/6524055002_DO002.xlsx', f"{output_relative_dir}/income_distribution.xlsx")
+
+# doanload population statistics from ABS
+urlretrieve('https://www.abs.gov.au/statistics/people/population/regional-population-age-and-sex/2021/32350DS0001_2021.xlsx', f"{output_relative_dir}/population.xlsx")
+
+# doanload school locations
+urlretrieve('https://www.education.vic.gov.au/Documents/about/research/datavic/dv331_schoollocations2022.csv', f"{output_relative_dir}/schools.csv")
--- scripts/scrape.py
+++ scripts/scrape.py
@@ -15,6 +15,10 @@ from collections import defaultdict
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
+import requests
+
+headers = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppelWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
+
 # constants
 BASE_URL = "https://www.domain.com.au"
 N_PAGES = range(1, 51) # update this to your liking
@@ -26,7 +30,7 @@ property_metadata = defaultdict(dict)
 # generate list of urls to visit
 for page in N_PAGES:
     url = BASE_URL + f"/rent/melbourne-region-vic/?sort=price-desc&page={page}"
-    bs_object = BeautifulSoup(urlopen(url), "lxml")
+    bs_object = BeautifulSoup(requests.get(url, headers = headers).text, "html.parser")
 
     # find the unordered list (ul) elements which are the results, then
     # find all href (a) tags that are from the base_url website.
@@ -48,7 +52,8 @@ for page in N_PAGES:
 
 # for each url, scrape some basic metadata
 for property_url in url_links[1:]:
-    bs_object = BeautifulSoup(urlopen(property_url), "lxml")
+    bs_object = BeautifulSoup(requests.get(url, headers = headers).text, "html.parser")
+    
 
     # looks for the header class to get property name
     property_metadata[property_url]['name'] = bs_object \
